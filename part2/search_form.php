d18aaae67814707c219a348fb2eedf8e65a707e4
LachlanColville <S3897401@student.rmit.edu.au>
2026-05-21 23:11:41 +1000

    changes

---
+++ part2/search_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Car Search</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>Search for a Car</h1>
+    <form method="GET" action="search_result.php">
+        <label for="model">Enter Model:</label>
+        <input type="text" name="model" id="model">
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
